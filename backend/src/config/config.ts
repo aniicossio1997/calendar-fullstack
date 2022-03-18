99f8c4029710326d6c64b60015f6542022df504e
ana cossio <anacossio7@gmail.com>
2022-03-18 16:00:03 -0300

    se configuro la parte de mongod

---
+++ backend/src/config/config.ts
@@ -0,0 +1,19 @@
+import { config } from "dotenv";
+config();
+// export default {
+//   DB: {
+//     URI: process.env.MONGODB_URI || "mongodb://localhost:27017/calendar",
+//     USER: "",
+//     PASSWORD: "",
+//   },
+//   jwtSecret: process.env.SECRET_JWT || "somesecrettoken",
+// };
+export default {
+  DB: {
+    MONGO_DATABASE: process.env.MONGO_DATABASE || "calendar",
+    MONGO_USER: process.env.MONGO_USER || "",
+    MONGO_PASSWORD: process.env.MONGO_PASSWORD || "",
+    MONGO_HOST: process.env.MONGO_HOST || "localhost:27017",
+  },
+  jwtSecret: process.env.SECRET_JWT || "somesecrettoken",
+};
